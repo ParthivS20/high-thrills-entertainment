c52495843e11300b8aff3493e61eaf2131c05bea
Parthiv Shah <42523433+ParthivS20@users.noreply.github.com>
2020-11-23 14:53:30 -0600

    a 1

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,12 +5,34 @@ import "../styles.css";
 import Title from "../Components/Title";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   try {
     document.getElementById("navUl").classList.add("navUlHide");
   } catch (e) {}
   return (
     <div className="">
       <Title title="Login" />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
     </div>
   );
 }
